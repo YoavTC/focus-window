82ff40266442d5f165142b90cec1414ed218fb6f
Yoav TC <yoavtc2004@gmail.com>
2026-02-10 22:45:34 +0200

    Add existing code

--- src/focus-window.ts
+++ src/focus-window.ts
@@ -1,7 +1,21 @@
-import { showHUD, Clipboard } from "@raycast/api";
+import { showHUD, WindowManagement } from "@raycast/api";
+import { exec } from "child_process";
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default async function main() {
-  const now = new Date();
-  await Clipboard.copy(now.toLocaleDateString());
-  await showHUD("Copied date to clipboard");
+	const activeWindow = await WindowManagement.getActiveWindow();
+	
+	// Minimize all windows
+	exec(
+    	`powershell -NoProfile -Command "$shell = New-Object -ComObject Shell.Application; $shell.MinimizeAll()"`
+  	);
+
+	await showHUD("Focusing '" + activeWindow.id + "'");
+
+	// Restore using hwnd and the windows API (https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow)
+	const hwnd = activeWindow.id;
+	exec(
+		`powershell -NoProfile -Command "Add-Type 'using System; using System.Runtime.InteropServices; public class W{[DllImport(\\\"user32.dll\\\")] public static extern bool ShowWindow(IntPtr h,int c);}'; [W]::ShowWindow([IntPtr]${hwnd},9)"`
+	);
 }
